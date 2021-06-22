f4bc63033840607cc22c076a2b7a698e774eaeb5
Víctor Moreno <victorgmp@gmail.com>
2021-06-21 21:27:45 -0400

    initialize api proxy

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,48 @@
-import { ServiceBase, Configuration } from 'polymetis-node';
+import { ServiceBase, Configuration, ServiceResources } from 'polymetis-node';
+import * as _ from 'lodash';
+import proxy from 'http-proxy-middleware';
+import cors from 'cors';
 
 // Initializing service
 const configuration: Partial<Configuration> = {
   baseDir: __dirname,
 };
-
 const service = new ServiceBase({ configuration });
 
 service.init()
   .then(async () => {
-    await service.initTasks();
-    await service.initEvents();
-    await service.initRPCs();
-    await service.initAPI();
+    service.logger.info('Setting proxy middleware options...');
+    const routes = [
+      {
+        route: '/auth',
+        target: 'http://localhost:8001/api',
+      },
+      {
+        route: '/users',
+        target: 'http://localhost:8002/api',
+      },
+    ];
 
-    service.logger.info('Service online on pid', process.pid);
+    service.apiApp.use(cors());
+
+    for (const { route, target } of routes) {
+      service.apiApp.use(
+        route,
+        proxy({
+          target,
+          changeOrigin: true,
+          pathRewrite: (path: string) => {
+            const afterPath = path.split('?').slice(1).join('?');
+            const retPath = path.split('?')[0].split('/').slice(2).join('/');
+            return `${retPath}${afterPath === '' ? '' : '?'}${afterPath}`;
+          },
+          logLevel: 'debug', //  ['debug', 'info', 'warn', 'error', 'silent']. Default: 'info'
+        }),
+      );
+      service.logger.info('- ', route, '=>', target);
+    }
+
+    await service.initAPI();
   })
   .catch((error: any) => {
     service.logger.error('Exiting', error);
